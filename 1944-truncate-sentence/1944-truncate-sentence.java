3aaa0ac2d211581435c9779b2b12573a416ca777
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-10-01 23:58:47 +0530

    Time: 0 ms (100%), Space: 40.5 MB (82.17%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.java
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -1,12 +1,19 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-        String arr[] = s.split(" ");
+       /* String arr[] = s.split(" ");
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < k; i++) {
             if(i < arr.length) {
                 sb.append(arr[i]).append(" ");
             }
         }
-        return sb.toString().trim();
+        return sb.toString().trim();*/
+        int count = 0 , i = 0;
+        for(i=0; i<s.length(); i++){
+            if(s.charAt(i)==' ') count++;
+            if(count==k) break;
+        }
+        if(i==s.length()) return s;
+        return s.substring(0,i);
     }
 }
