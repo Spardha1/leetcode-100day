1f618be17550982f5847c910197f11cc4b3ef1ec
Spardha1 <80118289+Spardha1@users.noreply.github.com>
2023-11-27 23:44:10 +0530

    Updated Solution - GfG to GitHub

--- Basic/Change the string/change-the-string.java
+++ Basic/Change the string/change-the-string.java
@@ -26,6 +26,11 @@ class GfG
 
 //User function Template for Java
 
+
+
+
+//User function Template for Java
+
 class Solution{
     String modify(String s){
         if (Character.isUpperCase(s.charAt(0))) 
