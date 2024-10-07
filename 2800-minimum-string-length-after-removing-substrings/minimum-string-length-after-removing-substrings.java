3317ac87cacdcd63cf348bad26620e909e543d96
Spardha Kumari <80118289+Spardha1@users.noreply.github.com>
2024-10-07 11:47:40 +0530

    Time: 4 ms (85.00%) | Memory: 42.7 MB (98.52%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,15 +1,40 @@
 class Solution {
     public int minLength(String s) {
 
-        while(s.contains("AB")|| (s.contains("CD"))){
-            if(s.contains("AB")){
-                s= s.replace("AB","");
+        // while(s.contains("AB")|| (s.contains("CD"))){
+        //     if(s.contains("AB")){
+        //         s= s.replace("AB","");
+        //     }
+        //     else if (s.contains("CD")){
+        //         s=s.replace("CD","");
+        //     }
+        // }
+        // return s.length();
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char currentCharacter = s.charAt(i);
+
+            if(st.isEmpty()){
+                st.push(s.charAt(i));
+                continue;
+               
             }
-            else if (s.contains("CD")){
-                s=s.replace("CD","");
+
+            if(currentCharacter=='B' && st.peek()=='A'){
+                st.pop();
             }
+            else if(currentCharacter == 'D' && st.peek()=='C'){
+                st.pop();
+            }
+            else{
+                st.push(currentCharacter);
+            }
+
+
         }
-        return s.length();
+        return st.size();
         
     }
 }
