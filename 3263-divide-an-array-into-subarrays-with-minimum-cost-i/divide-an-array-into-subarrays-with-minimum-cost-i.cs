a9928b9a711e738fff8179c4fd8648b082dd00b2
Spardha Kumari <80118289+Spardha1@users.noreply.github.com>
2026-02-02 00:21:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (66.67%) - LeetSync

---
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public int MinimumCost(int[] nums)
+    {
+        int min1 = 100;
+        int min2 = 100;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < min1)
+            {
+                min2 = min1;
+                min1 = nums[i];
+            }
+            else if (nums[i] < min2)
+            {
+                min2 = nums[i];
+            }
+        }
+
+        return nums[0] + min1 + min2;
+    }
+}
+        
